10202a8f626d8190dc8218e433d16cba3266a710
Apoorv Agarwal <150249267+Apoorv-AK-Coder@users.noreply.github.com>
2023-12-19 16:50:57 +0530

    fds

--- apikey.js
+++ apikey.js
@@ -3,7 +3,19 @@ let p = fetch("https://api.themoviedb.org/3/discover/movie?api_key=7ee6bd2b1a240
 p.then((response) => {
     console.log(response.status)
     console.log(response.ok)
-    return response.json()
+    return response.result()
 }).then((value2) => {
     console.log(value2)
 })
+
+let elm = document.getElementById("intro");
+
+let html = fetch("https://api.themoviedb.org/3/discover/movie?api_key=7ee6bd2b1a240379b50d2b8ac4ddfeb0");
+console.log(elm);
+// elm.insertAdjacentHTML('beforebegin', html);
+
+// elm.insertAdjacentHTML('afterbegin', html);
+
+elm.insertAdjacentHTML('beforeend', html);
+
+// elm.insertAdjacentHTML('afterend', html);
